0ccc68061fb9e3e36a7226bfb7580aa8144dd811
Kiran Dikshit <kirandikshit124@gmail.com>
2026-08-17 13:43:01 +0530

    using context to take user data

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,16 @@
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <AuthProvider>
+            <Toaster position="top-center" />
+            <App />
+        </AuthProvider>
+    </BrowserRouter>
+);
