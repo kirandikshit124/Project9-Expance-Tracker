f1e9aca53d1056759f9f8dcacc835665a8c74ef5
Kiran Dikshit <kirandikshit124@gmail.com>
2026-08-28 12:39:30 +0530

    final6 changes

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,10 @@ const dashboardRouter = require('./routes/dashboardRouter');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://spendly-assistant.onrender.com',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
